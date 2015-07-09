26bd29d6f47943905eefa2bb2447225d3c11510e
Eric Constantinides <eric@flickerbox.com>
2015-07-09 02:48:51 -0700

    Add immediately invoked function expression to unclutter the global namespace

--- responsive-move.js
+++ responsive-move.js
@@ -21,32 +21,35 @@
 //		.js-responsive-move--is-inactive
 //			Added to the element which is currently inactive
 //
+//		methods:
+//			resize
+//			set
 //------------------------------------------------------------------------------
-function responsiveMove() {
-
-	var $items = $('.js-responsive-move');
-	$items.each(function(index, el) {
-		function responsiveMoveGo() {
-			if (window.matchMedia && window.matchMedia('(max-width: ' + breakpoint + 'px)').matches) {
-				$item.removeClass('js-responsive-move--is-active').addClass('js-responsive-move--is-inactive').html('');
-				$target.removeClass('js-responsive-move--is-inactive').addClass('js-responsive-move--is-active').html(content);
-			} else {
-				$target.removeClass('js-responsive-move--is-active').addClass('js-responsive-move--is-inactive').html('');
-				$item.removeClass('js-responsive-move--is-inactive').addClass('js-responsive-move--is-active').html(content);
-			}
-		}
-		var $item = $(this);
-		if ( $item.attr('data-js-responsive-move-breakpoint')  && !isNaN($item.attr('data-js-responsive-move-breakpoint')) ) {
-			var breakpoint = $item.attr('data-js-responsive-move-breakpoint');
+!function($){
+	"use strict";
+	var items = document.getElementsByClassName('js-responsive-move');
+	function responsiveMoveGo() {
+		if (window.matchMedia && window.matchMedia('(max-width: ' + breakpoint + 'px)').matches) {
+			$item.removeClass('js-responsive-move--is-active').addClass('js-responsive-move--is-inactive').html('');
+			$target.removeClass('js-responsive-move--is-inactive').addClass('js-responsive-move--is-active').html(content);
 		} else {
-			var breakpoint = 767;
+			$target.removeClass('js-responsive-move--is-active').addClass('js-responsive-move--is-inactive').html('');
+			$item.removeClass('js-responsive-move--is-inactive').addClass('js-responsive-move--is-active').html(content);
 		}
+	}
+	for ( var i = 0; i < items.length; i++ ) {
+		var item = items[i];
+		var $item = $(items[i]);
+		if ( item.getAttribute('data-js-responsive-move-breakpoint') && !isNaN(item.getAttribute('data-js-responsive-move-breakpoint')) ) {
+			breakpoint = item.getAttribute('data-js-responsive-move-breakpoint');
+		}
+		var targetEl = item.getAttribute('data-js-responsive-move');
 		var $target = $('[data-js-responsive-move="' + $item.attr('data-js-responsive-move') + '"]:not(.js-responsive-move)');
 		var content = $item.html();
 		responsiveMoveGo();
-		$(window).resize(function() {
+		window.addEventListener('resize', function(event){
 			responsiveMoveGo();
 		});
-	});
-}
-responsiveMove();
+	}
+}(jQuery);
+
